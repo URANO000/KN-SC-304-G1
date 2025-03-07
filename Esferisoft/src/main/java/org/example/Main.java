8d63f8b7c3cbc79991129a15a312b8783fc638b1
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-03-07 16:43:35 -0600

    Update Main.java

--- Esferisoft/src/main/java/org/example/Main.java
+++ Esferisoft/src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
-import org.example.Mod0.FuncionConfig;
+import org.example.Mod0.Menu;
 import org.example.Mod1Cajas.ManagerCajas;
 
 public class Main {
     public static void main(String[] args) {
         //Creamos los nuevos objetos de los diferentes modulos
-        FuncionConfig funcionConfig = new FuncionConfig();
         ManagerCajas managerCajas = new ManagerCajas();
+        Menu menu = new Menu();
+        menu.mostrarMenu();
 
         funcionConfig.config();
         managerCajas.menuCajas();
