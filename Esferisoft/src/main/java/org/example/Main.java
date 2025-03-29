b390a480428e7b0363d9b2cfd70f560ea975579e
mhlebens <marifermatah@gmail.com>
2025-03-28 22:45:33 -0600

    Subí la llamada del módulo 1.2 al main

--- Esferisoft/src/main/java/org/example/Main.java
+++ Esferisoft/src/main/java/org/example/Main.java
@@ -5,17 +5,25 @@ import org.example.Mod1Cajas.ManagerCajas;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import org.example.Mod0.ConfigSucursal;
+import org.example.Mod1Cajas.ListaCajas;
+import org.example.Mod2Atencion.ManagerAtencion;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        //Creamos los nuevos objetos de los diferentes modulos
-        ManagerCajas managerCajas = new ManagerCajas();
-
+        // Crear objetos
         Menu menu = new Menu();
-
         menu.mostrarMenu();
+
+        ManagerCajas managerCajas = new ManagerCajas();
         managerCajas.menuCajas();
 
+        // Recuperar los objetos necesarios
+        ListaCajas listaCajas = managerCajas.getListaCajas();
+        ConfigSucursal config = menu.getConfiguracion();
 
+        // Usar ManagerAtencion con los datos correctos
+        ManagerAtencion managerAtencion = new ManagerAtencion(listaCajas, config.getNombreSucursal());
+        managerAtencion.mostrarMenuAtencion();
     }
 }
