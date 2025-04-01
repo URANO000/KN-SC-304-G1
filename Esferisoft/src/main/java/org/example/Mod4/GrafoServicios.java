2b2fccba99f7e0715f8754611d978a5350c02812
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-04-01 11:43:48 -0600

    Update GrafoServicios.java

--- Esferisoft/src/main/java/org/example/Mod4/GrafoServicios.java
+++ Esferisoft/src/main/java/org/example/Mod4/GrafoServicios.java
@@ -18,6 +18,27 @@ public class GrafoServicios {
     private static final String ARCHIVO = "grafo.txt";
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+ public String obtenerGrafoComoTexto() {
+    StringBuilder resultado = new StringBuilder("Trámites y sus servicios:\n");
+    NodoTramite actual = inicio;
+
+    if (actual == null) {
+        return "El grafo está vacío.";
+    }
+
+    while (actual != null) {
+        resultado.append("- ").append(actual.nombre).append("\n");
+        NodoServicio servicioActual = actual.primerServicio;
+        while (servicioActual != null) {
+            resultado.append("  * ").append(servicioActual.nombre).append("\n");
+            servicioActual = servicioActual.siguienteServicio;
+        }
+        actual = actual.siguienteTramite;
+    }
+
+    return resultado.toString();
+}
+
     private static class NodoTramite {
         String nombre;
         NodoServicio primerServicio;
